987db5e8109c94771149c04d6c4daa886a48053d
Uilque Messias <uqmessias@gmail.com>
2020-01-29 17:33:12 -0300

    feat(ws): setup WebSocket server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # web-socket-react-native
 POC project using WebSocket server based on deno and the client based on React Native
+
+## Running WebSocket on [Deno](https://deno.land)
+
+First of all, follow the instal instructions at [Deno Land](https://deno.lan).
+
+After you have installed it, you need to start the `WebSocket` and you can do that by running the following:
+
+```bash
+# If everthing went sucessfuly, you must see the message:
+# "WebSocket server is running on :8008"
+# it means the server is running on "ws://localhost:8080"
+deno --allow-net ./web-socket-server/index.ts
+```
---
+++ web-socket-server/index.ts
@@ -0,0 +1,58 @@
+import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent
+} from "https://deno.land/std/ws/mod.ts";
+
+const handler = async (portArg: string) => {
+  const port = Deno.args[1] || (typeof portArg === "string" ? portArg : "80");
+
+  const connections = [];
+
+  const intervalId = setInterval(async () => {
+    for await (const sock of connections) {
+      await sock.send(new Date(Date.now()).toISOString());
+    }
+  }, 2);
+
+  console.log(`WebSocket server is running on :${port}`);
+
+  await listenAndServe(`:${port}`, async req => {
+    const { headers, conn } = req;
+    try {
+      const sock = await acceptWebSocket({
+        conn,
+        headers,
+        bufReader: req.r,
+        bufWriter: req.w
+      });
+      console.log("socket connected!");
+      connections.push(sock);
+      const it = sock.receive();
+      while (true) {
+        try {
+          const { done, value } = await it.next();
+          if (done) {
+            break;
+          }
+          const ev = value;
+          if (isWebSocketCloseEvent(ev)) {
+            // close
+            const { code, reason } = ev;
+            const index = connections.indexOf(sock);
+            connections.splice(index, 1);
+            console.log("ws:Close", code, reason);
+          }
+        } catch (e) {
+          console.error(`failed to receive frame: ${e}`);
+          await sock.close(1000).catch(console.error);
+        }
+      }
+    } catch (err) {
+      console.error(`failed to accept WebSocket: ${err}`);
+      clearInterval(intervalId);
+    }
+  });
+};
+
+handler("8080");
